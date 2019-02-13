87f814ffb2d76da27e42f83818273a2994f48c4b
vagrant <djcfield@hotmail.com>
2019-02-13 23:28:27 +0000

    Fixed broken code

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,7 +18,7 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public unction __construct(){
+	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
 	}
